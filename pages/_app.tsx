7ae2663dfd712873dcd509d8757f2bed1ea28a76
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2021-08-08 16:39:38 -0300

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => setLoading(false));
   useEffect(() => void navigator.serviceWorker.register("/sw.js"));
   return <Component {...pageProps} />;
 }
